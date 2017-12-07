32f9bcd2b14d7ab8ba52d0f56749e16654b4ac2b
Marshajv <Marshajv@gmatc.matc.edu>
2017-12-06 19:40:45 -0600

    added post processing and enemy stuff

---
+++ Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseEnemy : MonoBehaviour {
+
+	public float enemyMoveSpeed;
+	public List<GameObject> positionList = new List<GameObject>();
+	//change to private later
+	public bool wasHit = false;
+
+	public virtual void Start()
+	{
+
+	}
+	public virtual void Update()
+	{
+
+	}
+
+	public void EnemyMovement()//movement pattern of the enemies
+	{
+
+	}
+	public void EnemyDeath(bool _wasHit, GameObject _deadObject)//when enemy is hit
+	{
+		if (_wasHit.Equals(true))
+			Debug.Log("send true message");
+		else
+			Debug.Log("send false message to spawner");
+		//have to send a message to the enemy spawner
+	}
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+	public int missesInARow = 0;
+	public List<GameObject> positionsList = new List<GameObject>();
+	public int maxNumActive;
+	
+	public List<GameObject> occupiedPositions = new List<GameObject>();
+
+	private bool gameOver = false, shootyGame = false;//shootyGame is if the game type is shooting or whack a mole
+	private bool needSpawns = false;
+	//private bool isMoveable = false; may not need from the check in spawn enemies
+
+	void Start () {
+		
+	}
+	private void StartGame()
+	{
+		int i = HowManyToActivate(maxNumActive);
+		for (int j = 0; j < i; j++)
+		{
+			SpawnEnemy(Random.Range(0, positionsList.Count-1));
+		}
+	}
+	private void SpawnEnemy(int _index)
+	{
+		Debug.Log(_index);
+		//make a class on the enemies that contains a public bool
+		//the bool will determine if the enemy can move or not
+		//check if the enemy can move
+		//determine game mode from available enemy types
+	}
+	private int HowManyToActivate(int _maxAlive)
+	{
+		int _count = Random.Range(1, _maxAlive);
+		return _count;
+	}
+	void Update () {
+		
+	}
+}
